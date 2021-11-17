56fb1f189f5bb4f467f1eac497f375c9a1a08ee4
joacozimer <joacozimeran@gmail.com>
2021-11-17 10:15:29 -0300

    initial commit

---
+++ src/database.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import config from "./config";
+
+(async () => {
+  try {
+    const db = await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+    console.log("DB is connected to", db.connection.host);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
